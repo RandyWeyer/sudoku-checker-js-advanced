4809084b32b1a3121fa93beba31efa07b72aeade
Colin Bates and Randy Weyer <colin@>
2018-08-01 11:31:24 -0700

    Fix arrays to always slice before they sort

--- spec/commit-sudoku-spec.js
+++ spec/commit-sudoku-spec.js
@@ -3,6 +3,7 @@ import { Sudoku } from '../src/sudoku';
 describe('Sudoku', function() {
 
   it('will check for row sudoku', function() {
+    console.log(grid);
     var grid = [
       [8,2,4,9,5,3,6,7,1],
       [6,3,5,8,1,7,9,2,4],
@@ -15,7 +16,8 @@ describe('Sudoku', function() {
       [9,5,3,1,7,8,4,6,2]
     ];
     var newSudoku = new Sudoku(grid);
-    expect(newSudoku.rowCzech()).toEqual(true);
+    console.log(grid);
+    expect(newSudoku.checkWin()).toEqual(true);
   });
   it('will check for column sudoku', function() {
     var grid = [
@@ -30,7 +32,7 @@ describe('Sudoku', function() {
       [9,5,3,1,7,8,4,6,2]
     ];
     var newSudoku = new Sudoku(grid);
-    expect(newSudoku.rowCzech()).toEqual(true);
+    expect(newSudoku.checkWin()).toEqual(true);
   });
   it('will check for cell sudoku', function() {
     var grid = [
@@ -45,6 +47,99 @@ describe('Sudoku', function() {
       [9,5,3,1,7,8,4,6,2]
     ];
     var newSudoku = new Sudoku(grid);
-    expect(newSudoku.cellCzech()).toEqual(true);
+    expect(newSudoku.checkWin()).toEqual(true);
+  });
+  it('should test if board repeats horizontally', function(){
+    var grid = [
+      [1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ];
+    var newSudoku = new Sudoku(grid);
+    expect(newSudoku.checkWin()).toEqual(false);
+  });
+  it('should test if board does not repeat horizontally', function(){
+    // for (var i = 0; i < 4; i++) {
+    //   console.log("I have looped " + i);
+    // }
+    var grid = [
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9]
+    ];
+    var newSudoku = new Sudoku(grid);
+    expect(newSudoku.checkWin()).toEqual(false);
+  });
+  it('should test if board repeats vertically', function(){
+    var grid = [
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9]
+    ];
+    var newSudoku = new Sudoku(grid);
+    expect(newSudoku.checkWin()).toEqual(false);
+  });
+  it('should test if board does not repeat vertically', function(){
+    var grid = [
+      [1,2,3,4,5,6,7,8,9],
+      [2,3,4,5,6,7,8,9,1],
+      [3,4,5,6,7,8,9,1,2],
+      [4,5,6,7,8,9,1,2,3],
+      [5,6,7,8,9,1,2,3,4],
+      [6,7,8,9,1,2,3,4,5],
+      [7,8,9,1,2,3,4,5,6],
+      [8,9,1,2,3,4,5,6,7],
+      [9,1,2,3,4,5,6,7,8]
+    ];
+    var newSudoku = new Sudoku(grid);
+    expect(newSudoku.checkWin()).toEqual(false);
+  });
+  it('should test a sudoku from wikipedia', function(){
+    var grid = [
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9]
+    ];
+    var newSudoku = new Sudoku(grid);
+    expect(newSudoku.checkWin()).toEqual(true);
+  });
+  it('should test a sudoku from previously', function(){
+    var grid = [
+      [8,2,4,9,5,3,6,7,1],
+      [6,3,5,8,1,7,9,2,4],
+      [7,1,9,6,2,4,8,5,3],
+      [5,8,7,2,9,1,3,4,6],
+      [1,4,2,7,3,6,5,8,9],
+      [3,9,6,4,8,5,2,1,7],
+      [2,6,1,5,4,9,7,3,8],
+      [4,7,8,3,6,2,1,9,5],
+      [9,5,3,1,7,8,4,6,2]
+    ];
+    var newSudoku = new Sudoku(grid);
+    expect(newSudoku.checkWin()).toEqual(true);
   });
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -4,9 +4,8 @@ export function Sudoku(grid){
 var compareArray = [1,2,3,4,5,6,7,8,9];
 
 Sudoku.prototype.rowCzech = function(){
-  var checkArray = this.grid.slice();
-  for(var i = 0; i < checkArray.length; i++){
-    var row = checkArray[i].sort();
+  for(var i = 0; i < this.grid.length; i++){
+    var row = this.grid[i].slice().sort();
     for(var j = 0; j < row.length; j++){
       if(row[j] != compareArray[j]){
         return false;
@@ -22,10 +21,10 @@ Sudoku.prototype.columnCzech = function(){
       columns.push(this.grid[j][i]);
     }
   }
-  for(var k = 0; k < columns.length; k++){
-    var row = columns[k].sort();
+  for(var k = 0; k < this.grid.length; k++){
+    var row = this.grid[k].slice().sort();
     for(var f = 0; f < row.length; f++){
-      if(row[f] != columns[f]){
+      if(row[f] != compareArray[f]){
         return false;
       }
     }
@@ -34,111 +33,53 @@ Sudoku.prototype.columnCzech = function(){
 };
 
 Sudoku.prototype.cellCzech = function(){
- var flag;
+  var flag = false;
   for(var k=0;k<3;k++){
     for(var m=0;m<3;m++){
-    flag=this.checkGrid(k*3,m*3);
-   if(flag===false) return false;
- }
-}
+      flag=this.checkGrid(k*3,m*3);
+      if(flag == false) {return false}
+    }
+  }
   return true;
 };
 
 Sudoku.prototype.checkGrid = function(i,j){
-    var cells = [];
-    for(var k=0;k<3;k++){
-      for(var m=0;m<3;m++){
-        var a = i+k;
-        var b = j+m;
-        cells.push(this.grid[a][b]);
-      }
-    }
-    var row9 = cells.sort();
-    for(var t = 0; t < row9.length; t++){
-      if(row9[t] != compareArray[t]){
-        return false;
-      }
-
-}
-return true;
-}
-/*
-Sudoku.prototype.cellCzech = function(){
   var cells = [];
-  var row = [];
-  var row1 = [];
-  var row2 = [];
-  var row3 = [];
-  var row4 = [];
-  var row5 = [];
-  var row6 = [];
-  var row7 = [];
-  var row8 = [];
+  for(var k=0;k<3;k++){
+    for(var m=0;m<3;m++){
+      var a = i+k;
+      var b = j+m;
+      cells.push(this.grid[a][b]);
+    }
+  }
+  var row9 = cells.slice().sort();
+  for(var t = 0; t < row9.length; t++){
+    if(row9[t] != compareArray[t]){
+      return false;
+    }
 
-  for(var a = 0; a < 3; a++){
-    for(var b = 0; b < 3; b++){
-      row.push(this.grid[a][b]);
-    }
-  }
-  cells.push(row);
-  for(var c = 0; c < 3; c++){
-    for(var d = 3; d < 6; d++){
-      row1.push(this.grid[c][d]);
-    }
-  }
-  cells.push(row1);
-  for(var e = 0; e < 3; e++){
-    for(var f = 6; f < 9; f++){
-      row2.push(this.grid[e][f]);
-    }
-  }
-  cells.push(row2);
-  for(var g = 3; g < 6; g++){
-    for(var h = 0; h < 3; h++){
-      row3.push(this.grid[g][h]);
-    }
-  }
-  cells.push(row3);
-  for(var i = 3; i < 6; i++){
-    for(var j = 3; j < 6; j++){
-      row4.push(this.grid[i][j]);
-    }
-  }
-  cells.push(row4);
-  for(var k = 3; k < 6; k++){
-    for(var l = 6; l < 9; l++){
-      row5.push(this.grid[k][l]);
-    }
-  }
-  cells.push(row5);
-  for(var m = 6; m < 9; m++){
-    for(var n = 0; n < 3; n++){
-      row6.push(this.grid[m][n]);
-    }
-  }
-  cells.push(row6);
-  for(var o = 6; o < 9; o++){
-    for(var p = 3; p < 6; p++){
-      row7.push(this.grid[o][p]);
-    }
-  }
-  cells.push(row7);
-  for(var q = 6; q < 9; q++){
-    for(var r = 6; r < 9; r++){
-      row8.push(this.grid[q][r]);
-    }
-  }
-  cells.push(row8);
-  /////////////////////////
-  console.log(cells);
-  for(var s = 0; s < cells.length; s++){
-    var row9 = cells[s].sort();
-    for(var t = 0; t < row9.length; t++){
-      if(row9[t] != compareArray[t]){
-        return false;
-      }
-    }
   }
   return true;
-};
-*/
+}
+Sudoku.prototype.checkWin = function(){
+  console.log(this);
+  var a = this.rowCzech();
+  var b = this.columnCzech();
+  var c = this.cellCzech();
+
+  if(!a){
+    console.log("Row Failed");
+  }
+  if(!b){
+    console.log("Column Failed");
+  }
+  if(!c){
+    console.log("Cell Failed");
+  }
+  if(a && b && c){
+    return true;
+  } else {
+
+    return false;
+  }
+}
