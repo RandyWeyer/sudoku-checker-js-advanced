2f66a9942e7393b01a60088ab07c5b422a0b93ab
Colin Bates and Randy Weyer <colin+randy@>
2018-08-02 16:37:28 -0700

    Several new methods

--- spec/commit-sudoku-spec.js
+++ spec/commit-sudoku-spec.js
@@ -1,10 +1,8 @@
 import { Sudoku } from '../src/sudoku';
 
 describe('Sudoku', function() {
-
   it('will check for row sudoku', function() {
-    console.log(grid);
-    var grid = [
+    let grid = [
       [8,2,4,9,5,3,6,7,1],
       [6,3,5,8,1,7,9,2,4],
       [7,1,9,6,2,4,8,5,3],
@@ -15,12 +13,11 @@ describe('Sudoku', function() {
       [4,7,8,3,6,2,1,9,5],
       [9,5,3,1,7,8,4,6,2]
     ];
-    var newSudoku = new Sudoku(grid);
-    console.log(grid);
+    let newSudoku = new Sudoku(grid);
     expect(newSudoku.checkWin()).toEqual(true);
   });
   it('will check for column sudoku', function() {
-    var grid = [
+    let grid = [
       [8,2,4,9,5,3,6,7,1],
       [6,3,5,8,1,7,9,2,4],
       [7,1,9,6,2,4,8,5,3],
@@ -31,11 +28,11 @@ describe('Sudoku', function() {
       [4,7,8,3,6,2,1,9,5],
       [9,5,3,1,7,8,4,6,2]
     ];
-    var newSudoku = new Sudoku(grid);
+    let newSudoku = new Sudoku(grid);
     expect(newSudoku.checkWin()).toEqual(true);
   });
   it('will check for cell sudoku', function() {
-    var grid = [
+    let grid = [
       [8,2,4,9,5,3,6,7,1],
       [6,3,5,8,1,7,9,2,4],
       [7,1,9,6,2,4,8,5,3],
@@ -46,11 +43,11 @@ describe('Sudoku', function() {
       [4,7,8,3,6,2,1,9,5],
       [9,5,3,1,7,8,4,6,2]
     ];
-    var newSudoku = new Sudoku(grid);
+    let newSudoku = new Sudoku(grid);
     expect(newSudoku.checkWin()).toEqual(true);
   });
   it('should test if board repeats everywhere', function(){
-    var grid = [
+    let grid = [
       [1, 1, 1, 1, 1, 1, 1, 1, 1],
       [1, 1, 1, 1, 1, 1, 1, 1, 1],
       [1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -61,11 +58,11 @@ describe('Sudoku', function() {
       [1, 1, 1, 1, 1, 1, 1, 1, 1],
       [1, 1, 1, 1, 1, 1, 1, 1, 1]
     ];
-    var newSudoku = new Sudoku(grid);
+    let newSudoku = new Sudoku(grid);
     expect(newSudoku.checkWin()).toEqual(false);
   });
   it('should test if board repeats horizontally', function(){
-    var grid = [
+    let grid = [
       [1, 1, 1, 1, 1, 1, 1, 1, 1],
       [2, 2, 2, 2, 2, 2, 2, 2, 2],
       [3, 3, 3, 3, 3, 3, 3, 3, 3],
@@ -76,14 +73,11 @@ describe('Sudoku', function() {
       [8, 8, 8, 8, 8, 8, 8, 8, 8],
       [9, 9, 9, 9, 9, 9, 9, 9, 9]
     ];
-    var newSudoku = new Sudoku(grid);
+    let newSudoku = new Sudoku(grid);
     expect(newSudoku.checkWin()).toEqual(false);
   });
   it('should test if board does not repeat horizontally', function(){
-    // for (var i = 0; i < 4; i++) {
-    //   console.log("I have looped " + i);
-    // }
-    var grid = [
+    let grid = [
       [1,2,3,4,5,6,7,8,9],
       [1,2,3,4,5,6,7,8,9],
       [1,2,3,4,5,6,7,8,9],
@@ -94,11 +88,11 @@ describe('Sudoku', function() {
       [1,2,3,4,5,6,7,8,9],
       [1,2,3,4,5,6,7,8,9]
     ];
-    var newSudoku = new Sudoku(grid);
+    let newSudoku = new Sudoku(grid);
     expect(newSudoku.checkWin()).toEqual(false);
   });
   it('should test if board repeats vertically', function(){
-    var grid = [
+    let grid = [
       [1,2,3,4,5,6,7,8,9],
       [1,2,3,4,5,6,7,8,9],
       [1,2,3,4,5,6,7,8,9],
@@ -109,11 +103,11 @@ describe('Sudoku', function() {
       [1,2,3,4,5,6,7,8,9],
       [1,2,3,4,5,6,7,8,9]
     ];
-    var newSudoku = new Sudoku(grid);
+    let newSudoku = new Sudoku(grid);
     expect(newSudoku.checkWin()).toEqual(false);
   });
   it('should test if board does not repeat vertically', function(){
-    var grid = [
+    let grid = [
       [1,2,3,4,5,6,7,8,9],
       [2,3,4,5,6,7,8,9,1],
       [3,4,5,6,7,8,9,1,2],
@@ -124,11 +118,11 @@ describe('Sudoku', function() {
       [8,9,1,2,3,4,5,6,7],
       [9,1,2,3,4,5,6,7,8]
     ];
-    var newSudoku = new Sudoku(grid);
+    let newSudoku = new Sudoku(grid);
     expect(newSudoku.checkWin()).toEqual(false);
   });
   it('should test a sudoku from wikipedia', function(){
-    var grid = [
+    let grid = [
         [5, 3, 4, 6, 7, 8, 9, 1, 2],
         [6, 7, 2, 1, 9, 5, 3, 4, 8],
         [1, 9, 8, 3, 4, 2, 5, 6, 7],
@@ -139,11 +133,11 @@ describe('Sudoku', function() {
         [2, 8, 7, 4, 1, 9, 6, 3, 5],
         [3, 4, 5, 2, 8, 6, 1, 7, 9]
     ];
-    var newSudoku = new Sudoku(grid);
+    let newSudoku = new Sudoku(grid);
     expect(newSudoku.checkWin()).toEqual(true);
   });
   it('should test a sudoku from previously', function(){
-    var grid = [
+    let grid = [
       [8,2,4,9,5,3,6,7,1],
       [6,3,5,8,1,7,9,2,4],
       [7,1,9,6,2,4,8,5,3],
@@ -154,7 +148,154 @@ describe('Sudoku', function() {
       [4,7,8,3,6,2,1,9,5],
       [9,5,3,1,7,8,4,6,2]
     ];
-    var newSudoku = new Sudoku(grid);
+    let newSudoku = new Sudoku(grid);
     expect(newSudoku.checkWin()).toEqual(true);
   });
+  it('will check for same number in row and column', function(){
+    let grid = [
+      [8,2,"",9,5,3,6,7,1],
+      [6,3,5,8,1,7,9,2,4],
+      [7,1,9,6,2,4,8,5,3],
+      [5,8,7,2,9,1,3,4,6],
+      [1,4,2,7,3,6,5,8,9],
+      [3,9,6,4,8,5,2,1,7],
+      [2,6,1,5,4,9,7,3,8],
+      [4,7,8,3,6,2,1,9,5],
+      [9,5,3,1,7,8,4,6,2]
+    ];
+    let testNo = 4;
+    let x = 0;
+    let y = 2;
+    let newSudoku = new Sudoku(grid);
+    expect(newSudoku.testRowAndColumn(testNo,x,y)).toEqual(true);
+  });
+  it('will check for same number in subgrid', function(){
+    let grid = [
+      [8,2,"",9,5,3,6,7,1],
+      [6,3,5,8,1,7,9,2,4],
+      [7,1,9,6,2,4,8,5,3],
+      [5,8,7,2,9,1,3,4,6],
+      [1,4,2,7,3,6,5,8,9],
+      [3,9,6,4,8,5,2,1,7],
+      [2,6,1,5,4,9,7,3,8],
+      [4,7,8,3,6,2,1,9,5],
+      [9,5,3,1,7,8,4,6,2]
+    ];
+    let testNo = 4;
+    let x = 0;
+    let y = 2;
+    let newSudoku = new Sudoku(grid);
+    expect(newSudoku.testSubGrid(testNo,x,y)).toEqual(true);
+  });
+  it('will display the possible solutions of a cell', function(){
+    let grid = [
+      [8,2,"",9,5,3,6,7,1],
+      [6,3,5,8,1,7,9,2,4],
+      [7,1,9,6,2,4,8,5,3],
+      [5,8,7,2,9,1,3,4,6],
+      [1,4,2,7,3,6,5,8,9],
+      [3,9,6,4,8,5,2,1,7],
+      [2,6,1,5,4,9,7,3,8],
+      [4,7,8,3,6,2,1,9,5],
+      [9,5,3,1,7,8,4,6,2]
+    ];
+    let x = 0;
+    let y = 2;
+    let expectedResult = [4];
+    let newSudoku = new Sudoku(grid);
+    expect(newSudoku.possibleSolutions(x,y)).toEqual(expectedResult);
+  });
+  it('will create a monster', function(){
+    let grid = [
+      [5,3,"","",7,"","","",""],
+      [6,"","",1,9,5,"","",""],
+      ["",9,8,"","","","",6,""],
+      [8,"","","",6,"","","",3],
+      [4,"","",8,"",3,"","",1],
+      [7,"","","",2,"","","",6],
+      ["",6,"","","","",2,8,""],
+      ["","","",4,1,9,"","",5],
+      ["","","","",8,"","",7,9]
+    ];
+    let expectedGrid = [
+      [[], [], [ 1, 2, 4 ], [ 2, 6 ], [], [ 2, 4, 6, 8 ], [ 1, 4, 8, 9 ], [ 1, 2, 4, 9 ], [ 2, 4, 8 ]],
+      [[], [ 2, 4, 7 ], [ 2, 4, 7 ], [], [], [], [ 3, 4, 7, 8 ], [ 2, 3, 4 ], [ 2, 4, 7, 8 ]],
+      [[ 1, 2 ], [], [], [ 2, 3 ], [ 3, 4 ], [ 2, 4 ], [ 1, 3, 4, 5, 7 ], [], [ 2, 4, 7 ]],
+      [[], [ 1, 2, 5 ], [ 1, 2, 5, 9 ], [ 5, 7, 9 ], [], [ 1, 4, 7 ], [ 4, 5, 7, 9 ], [ 2, 4, 5, 9 ], [] ],
+      [[], [ 2, 5 ], [ 2, 5, 6, 9 ], [], [ 5 ], [], [ 5, 7, 9 ], [ 2, 5, 9 ], []],
+      [ [], [ 1, 5 ], [ 1, 3, 5, 9 ], [ 5, 9 ], [], [ 1, 4 ], [ 4, 5, 8, 9 ], [ 4, 5, 9 ], [] ],
+      [[ 1, 3, 9 ], [], [ 1, 3, 4, 5, 7, 9 ], [ 3, 5, 7 ], [ 3, 5 ], [ 7 ], [], [], [ 4 ]],
+      [[ 2, 3 ], [ 2, 7, 8 ], [ 2, 3, 7 ], [], [], [], [ 3, 6 ], [ 3 ], []],
+      [[ 1, 2, 3 ], [ 1, 2, 4, 5 ], [ 1, 2, 3, 4, 5 ], [ 2, 3, 5, 6 ], [], [ 2, 6 ], [ 1, 3, 4, 6 ], [], []]
+    ]
+    let newSudoku = new Sudoku(grid);
+    expect(newSudoku.create3DMonster()).toEqual(expectedGrid);
+  });
+  it('will update the grid', function(){
+    let grid = [
+      [5,3,"","",7,"","","",""],
+      [6,"","",1,9,5,"","",""],
+      ["",9,8,"","","","",6,""],
+      [8,"","","",6,"","","",3],
+      [4,"","",8,"",3,"","",1],
+      [7,"","","",2,"","","",6],
+      ["",6,"","","","",2,8,""],
+      ["","","",4,1,9,"","",5],
+      ["","","","",8,"","",7,9]
+    ];
+    let expectedGrid = [
+      [5,3,"","",7,"","","",""],
+      [6,"","",1,9,5,"","",""],
+      ["",9,8,"","","","",6,""],
+      [8,"","","",6,"","","",3],
+      [4,"","",8,5,3,"","",1],
+      [7,"","","",2,"","","",6],
+      ["",6,"","","",7,2,8,4],
+      ["","","",4,1,9,"",3,5],
+      ["","","","",8,"","",7,9]
+    ]
+    let newSudoku = new Sudoku(grid);
+    expect(newSudoku.spliceSolutions()).toEqual(expectedGrid);
+  });
+  it('will solve the puzzle', function(){
+    let grid = [
+      [5,3,"","",7,"","","",""],
+      [6,"","",1,9,5,"","",""],
+      ["",9,8,"","","","",6,""],
+      [8,"","","",6,"","","",3],
+      [4,"","",8,"",3,"","",1],
+      [7,"","","",2,"","","",6],
+      ["",6,"","","","",2,8,""],
+      ["","","",4,1,9,"","",5],
+      ["","","","",8,"","",7,9]
+    ];
+    let expectedGrid = [
+      [5,3,4,6,7,8,9,1,2],
+      [6,7,2,1,9,5,3,4,8],
+      [1,9,8,3,4,2,5,6,7],
+      [8,5,9,7,6,1,4,2,3],
+      [4,2,6,8,5,3,7,9,1],
+      [7,1,3,9,2,4,8,5,6],
+      [9,6,1,5,5,7,2,8,4],
+      [2,8,7,4,1,9,6,3,5],
+      [3,4,5,2,8,6,1,7,9]
+    ]
+    let newSudoku = new Sudoku(grid);
+    expect(newSudoku.createBoard()).toEqual(expectedGrid);
+  });
+    it('will check for row sudoku', function() {
+      let grid = [
+        [8,2,4,9,5,3,6,7,1],
+        [6,3,5,8,1,7,9,2,4],
+        [7,1,9,6,2,4,8,5,3],
+        [5,8,7,2,9,1,3,4,6],
+        [1,4,2,7,3,6,5,8,9],
+        [3,9,6,4,8,5,2,1,7],
+        [2,6,1,5,4,9,7,3,8],
+        [4,7,8,3,6,2,1,9,5],
+        [9,5,3,1,7,8,4,6,2]
+      ];
+      let newSudoku = new Sudoku(grid);
+      expect(newSudoku.checkWin()).toEqual(true);
+    });
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -39,7 +39,9 @@ Sudoku.prototype.cellCzech = function(){
   for(var k=0;k<3;k++){
     for(var m=0;m<3;m++){
       flag=this.checkGrid(k*3,m*3);
-      if(flag == false) {return false}
+      if(flag == false) {
+        return false;
+      }
     }
   }
   return true;
@@ -62,9 +64,8 @@ Sudoku.prototype.checkGrid = function(i,j){
 
   }
   return true;
-}
+};
 Sudoku.prototype.checkWin = function(){
-  console.log(this);
   var a = this.rowCzech();
   var b = this.columnCzech();
   var c = this.cellCzech();
@@ -81,7 +82,104 @@ Sudoku.prototype.checkWin = function(){
   if(a && b && c){
     return true;
   } else {
-
     return false;
   }
-}
+};
+Sudoku.prototype.testRowAndColumn = function(testNo, rowNo, columnNo){
+  for(var j = 0; j < this.grid[rowNo].length; j++){
+    if(this.grid[rowNo][j] === testNo){
+      return false;
+    }
+  }
+  for(var i = 0; i < this.grid.length; i++){
+    if(this.grid[i][columnNo] === testNo){
+      return false;
+    }
+  }
+  return true;
+};
+Sudoku.prototype.testSubGrid = function(testNo, rowNo, columnNo){
+  var i = rowNo - rowNo%3;
+  var j = columnNo - columnNo%3;
+  var row = [];
+  for(var k = 0 + i; k < 3 + i; k++){
+    for(var m = 0 + j; m < 3 + j; m++){
+      row.push(this.grid[k][m]);
+    }
+  }
+  for(var s = 0; s < row.length; s++){
+    if(row[s] === testNo){
+      return false;
+    }
+  }
+  return true;
+};
+Sudoku.prototype.isEmpty = function(i,j){
+  for(var f = 1; f < 10; f++){
+    if(this.grid[i][j] === f){
+      return false;
+    }
+  }
+  return true;
+};
+Sudoku.prototype.possibleSolutions = function(i,j){
+  var xPosition = i;
+  var yPosition = j;
+  var solutions = [];
+  for(var z = 1; z < 10; z++){
+    if(this.testRowAndColumn(z, xPosition, yPosition) && this.testSubGrid(z, xPosition, yPosition)){
+      solutions.push(z);
+    }
+  }
+  return solutions;
+};
+Sudoku.prototype.create3DMonster = function(){
+  var monster = [];
+  for(var m = 0; m < this.grid.length; m++){
+    var tempRow = [];
+    for(var n = 0; n < this.grid.length; n++){
+      if(this.isEmpty(m,n)){
+        tempRow.push(this.possibleSolutions(m,n));
+      } else {
+        tempRow.push([]);
+      }
+    }
+    monster.push(tempRow);
+  }
+  return monster;
+};
+Sudoku.prototype.spliceSolutions = function(){
+  var monster = this.create3DMonster();
+  for(var s = 0; s < monster.length; s++){
+    for(var n = 0; n < monster[s].length; n++){
+      if(monster[s][n].length === 1){
+        this.grid[s].splice(n,1,monster[s][n][0]);
+      }
+    }
+  }
+  return this.grid;
+};
+Sudoku.prototype.createBoard = function(){
+  for(var i = 0; i < this.grid.length; i++){
+    for (var j = 0; j < this.grid.length; j++) {
+      if (this.isEmpty(i,j)){
+        for (var k = 0; k < this.possibleSolutions(i,j).length; k++) {
+          if(this.isEmpty(i,j)){
+            this.grid[i][j] = this.possibleSolutions(i,j)[k];
+            console.log(this.grid);
+            console.log(this.grid[i][j]);
+            if (this.checkWin()){
+              console.log("You Win");
+              return this.grid;
+            } else {
+              i++;
+              this.createBoard()
+              continue;
+            }
+          }
+
+        }
+      }
+    }
+  }
+};
